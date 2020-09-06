0bd89e2ce3930c0381c1f3a0d3c91f4ae32a4eee
Henry <huyearly@gmail.com>
2020-09-06 01:45:54 -0600

    Refactor anagram redis

--- anagram-backend/src/anagram/data/anagram.ts
+++ anagram-backend/src/anagram/data/anagram.ts
@@ -4,7 +4,7 @@ export class AnagramAPI {
    * Check anagram
    */
   public async storeAnagram(textA: string, textB: string) {
-    const anagramkey = generateAnagramKey(textA, textB);
+    const anagramkey = AnagramAPI.generateAnagramKey(textA, textB);
     const redis = new RedisAnagramAPI();
     redis.storeAnagram(anagramkey);
     return;
@@ -15,32 +15,33 @@ export class AnagramAPI {
   public async getTop10Anagrams() {
     const redis = new RedisAnagramAPI();
     const rawresult: any[] = await redis.getTop10Anagram();
-    return generateAnagramTop10(rawresult);
+    return AnagramAPI.generateAnagramTop10(rawresult);
   }
-}
-/**
- * Generate deteministic key for redis to store
- */
-function generateAnagramKey(textA: string, textB: string) {
-  if (textA < textB) {
-    return textA + "," + textB;
+  /**
+   * Generate deteministic key for redis to store
+   */
+  private static generateAnagramKey(textA: string, textB: string) {
+    if (textA < textB) {
+      return textA + "," + textB;
+    }
+    return textB + "," + textA;
   }
-  return textB + "," + textA;
-}
-/**
- * Generate top 10 anagram response
- */
-function generateAnagramTop10(result: any[]) {
-  const itemlist = [];
-  for (let i = 0; i < result.length; i += 2) {
-    const item = {
-      anagramText: result[i],
-      anagramCheckingCount: result[i + 1],
+
+  /**
+   * Generate top 10 anagram response
+   */
+  private static generateAnagramTop10(result: any[]) {
+    const itemlist = [];
+    for (let i = 0; i < result.length; i += 2) {
+      const item = {
+        anagramText: result[i],
+        anagramCheckingCount: result[i + 1],
+      };
+      itemlist.push(item);
+    }
+    return {
+      top10: itemlist,
+      timestamp: Math.floor(new Date().getTime() / 1000),
     };
-    itemlist.push(item);
   }
-  return {
-    top10: itemlist,
-    timestamp: Math.floor(new Date().getTime() / 1000),
-  };
 }
